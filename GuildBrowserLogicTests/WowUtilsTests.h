d8313e388c45d7cf0f91bfe420b5f12c0bab9097
Codemis <johnathan@missionaldigerati.com>
2012-10-17 21:22:32 -0700

    Added testing for WowUtils class.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Johnathan Pulos on 10/17/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
