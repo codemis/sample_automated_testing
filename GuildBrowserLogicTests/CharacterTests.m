b6d5ef9c0b9cab03d1cd04f5764636cf06229615
Bruce Hobbs <brucehobbs@engineeredsw.com>
2012-10-22 16:06:52 -0700

    Declare and initialize Character variable for later use in other testCreateCharacterFromDetailJson methods

--- GuildBrowserLogicTests/CharacterTests.m
+++ GuildBrowserLogicTests/CharacterTests.m
@@ -4,12 +4,13 @@
 
 @implementation CharacterTests {
     NSDictionary *_characterDetailJson;
+    Character *_testGuy;
 }
 
 -(void)setUp {
     NSURL *dataServiceURL =
-    [[NSBundle bundleForClass:self.class] URLForResource:@"character"
-                                           withExtension:@"json"];
+      [[NSBundle bundleForClass:self.class] URLForResource:@"character"
+         withExtension:@"json"];
     NSData *sampleData = [NSData dataWithContentsOfURL:dataServiceURL];
     NSError *error;
     id json = [NSJSONSerialization JSONObjectWithData:sampleData
@@ -17,8 +18,23 @@
                                                 error:&error];
     STAssertNotNil(json,@"Invalid test data");
     _characterDetailJson = json;
+    _testGuy =
+      [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
 }
 -(void)tearDown {
     _characterDetailJson = nil;
+}
+-(void)testCreateCharacterFromDetailJson {
+    Character *testGuy1 =
+      [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
+    STAssertNotNil(testGuy1,@"Could not create character from detail json");
+    Character *testGuy2 =
+      [[Character alloc] initWithCharacterDetailData:nil];
+    STAssertNotNil(testGuy2,@"Could not create character from nil data");
+}
+-(void)testCreateCharacterFromDetailJsonProps {
+    STAssertEqualObjects(_testGuy.thumbnail,
+                         @"borean-tundra/171/40508075-avatar.jpg",
+                         @"thumbnail URL is wrong");    
 }
 @end
